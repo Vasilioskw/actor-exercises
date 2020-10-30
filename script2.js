325720dbecbe9f3bcd835190138c495bca116724
Vasilios Kambouras <69370711+Vasilioskw@users.noreply.github.com>
2020-10-29 22:28:55 -0400

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,40 @@
+function actors(arrayOfActors){
+    for (let i = 1; i < arrayOfActors.length; i++){
+        console.log(arrayOfActors[i])
+    }
+}
+
+const actor1 = {
+First_Name: "Zach",
+Last_Name: "Galifianakis",
+ Age: 48,
+ yearly_earnings: 393394
+}
+
+const actor2 = {
+    First_Name: "Nia",
+    Last_Name: "Vardalos",
+     Age: 39,
+     yearly_earnings: 193394
+    }
+;
+const actor3 = {
+        First_Name: "Tina",
+        Last_Name: "Fey",
+         Age: 48,
+         yearly_earnings: 993394
+        }
+
+        const actor4 = {
+            First_Name: "Maria",
+            Last_Name: "Menounos",
+             Age: 64,
+             yearly_earnings: 3943394
+            }
+
+            const arrayOfActors = [actor1, actor2, actor3, actor4]
+
+
+            actors(arrayOfActors)
+
+            
